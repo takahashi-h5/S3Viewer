d404442026e1b6d0563c5c35848db14cdae79e36
takahashi-h5 <mori@confrage.com>
2020-12-16 19:02:58 +0900

    commit

--- render-process.js
+++ render-process.js
@@ -3,18 +3,21 @@ const s3 = new AWS.S3({apiVersion: '2006-03-01'})
 const execSync = require('child_process').execSync
 
 const BrowserWindow = require('electron').remote.BrowserWindow
-const path = require('path')
+const path = require('path');
+const { DirectoryService, DocDB } = require('aws-sdk');
+const { DiffieHellman } = require('crypto');
 
 const bucketsNameBtn = document.getElementById('bucket-name-button')
 const clearBtn = document.getElementById('bucket-name-clear')
 const buckets = document.getElementById('bucket-name-list')
+const objectkeyList = document.getElementById('objectkey-list')
 
 // ボタンクリック時
 bucketsNameBtn.addEventListener('click', function(event) {
   const result =  execSync('aws s3 ls --profile default')
   const arr = result.toString().split('\r\n')
   for(let i=0;i< arr.length; i++) {
-    buckets.innerHTML += '<a href="' + arr[i].substring(20) + '">' + arr[i].substring(20) + '</a><br>'
+    buckets.innerHTML += '<a href=\"#\" onClick=dirr(this.innerHTML);>' + arr[i].substring(20) + '</a><br>'
   }
   bucketsNameBtn.setAttribute('disabled', true)
   clearBtn.removeAttribute('disabled')
@@ -22,8 +25,19 @@ bucketsNameBtn.addEventListener('click', function(event) {
 
 // クリアボタンクリック時
 clearBtn.addEventListener('click',function(event) {
-  console.log('hogehoge')
   bucketsNameBtn.removeAttribute('disabled')
   clearBtn.setAttribute('disabled', true)
   buckets.innerHTML = ''
 })
+
+function dirr(hoge) {
+  console.log('hoge')
+  console.log(hoge)
+  const result =  execSync('aws s3 ls ' + hoge + ' --profile default')
+  console.log(result.toString().split('\r\n'))
+  const arr = result.toString().split('\r\n')
+  for(let i=0;i< arr.length; i++) {
+    objectkeyList.innerHTML += '<a href=\"#\" onClick=dirr(this.innerHTML);>' + arr[i].substring(20) + '</a><br>'
+  }
+
+}
