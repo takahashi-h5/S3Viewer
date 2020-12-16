2c0680fef95c2d8163460c494a5d1e32a38eba97
takahashi-h5 <mori@confrage.com>
2020-12-16 16:56:19 +0900

    commit

--- render-process.js
+++ render-process.js
@@ -5,65 +5,25 @@ const execSync = require('child_process').execSync
 const BrowserWindow = require('electron').remote.BrowserWindow
 const path = require('path')
 
-const manageWindowBtn = document.getElementById('bucket-name-button')
+const bucketsNameBtn = document.getElementById('bucket-name-button')
+const clearBtn = document.getElementById('bucket-name-clear')
 const buckets = document.getElementById('bucket-name-list')
 
-// ボタンクリック時に、レンダラープロセスから別ウィンドウを表示
-manageWindowBtn.addEventListener('click', function(event) {
-  /*const myNotification = new Notification('通知', {
-    body: '通知本文'
-  })
-  myNotification.onclick = () => {
-    console.log('Notification clicked')
-  }*/
-
+// ボタンクリック時
+bucketsNameBtn.addEventListener('click', function(event) {
   const result =  execSync('aws s3 ls --profile default')
-  // console.log(result.toString())
-  const arr = result.toString().split('\n')
+  const arr = result.toString().split('\r\n')
   for(let i=0;i< arr.length; i++) {
-    buckets.innerHTML += arr[i].substring(20) + '<br>'
+    buckets.innerHTML += '<a href="' + arr[i].substring(20) + '">' + arr[i].substring(20) + '</a><br>'
   }
-  manageWindowBtn.setAttribute('disabled', true)
-  // const modalPath = path.join('file://', __dirname, '/childwindow.html')
-  // win = new BrowserWindow({
-  //   width: 400,
-  //   height: 275,
-  //   webPreferences: {
-  //     nodeIntegration: false, // node API許可
-  //     contextIsolation: true
-  //   }
-  // })
-  // // win.on('resize', updateReply)
-  // // win.on('move', updateReply)
-  // // win.on('focus', hideFocusBtn)
-  // // win.on('blur', showFocusBtn)
-  // // win.on('close', function() {
-  // //   hideFocusBtn()
-  // //   win = null
-  // // })
-  // console.log(modalPath)
-  // win.loadURL(modalPath)
-  // // win.show()
+  bucketsNameBtn.setAttribute('disabled', true)
+  clearBtn.removeAttribute('disabled')
+})
 
-  // function updateReply() {
-  //   const manageWindowReply = document.getElementById('manage-window-reply')
-  //   const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
-  //   manageWindowReply.innerText = message
-  // }
-
-  // const focusModalBtn = document.getElementById('focus-on-modal-window')
-
-  // function showFocusBtn(btn) {
-  //   if (!win) return
-  //   focusModalBtn.classList.add('smooth-appear')
-  //   focusModalBtn.classList.remove('disappear')
-  //   focusModalBtn.addEventListener('click', function() {
-  //     win.focus()
-  //   })
-  // }
-
-  // function hideFocusBtn() {
-  //   focusModalBtn.classList.add('disappear')
-  //   focusModalBtn.classList.remove('smooth-appear')
-  // }
+// クリアボタンクリック時
+clearBtn.addEventListener('click',function(event) {
+  console.log('hogehoge')
+  bucketsNameBtn.removeAttribute('disabled')
+  clearBtn.setAttribute('disabled', true)
+  buckets.innerHTML = ''
 })
