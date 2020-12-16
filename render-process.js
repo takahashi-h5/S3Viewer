1d607f8ba1ad190c131b9488722037c617cd474a
takahashi-h5 <mori@confrage.com>
2020-12-16 23:45:18 +0900

    commit

--- render-process.js
+++ render-process.js
@@ -11,13 +11,14 @@ const bucketsNameBtn = document.getElementById('bucket-name-button')
 const clearBtn = document.getElementById('bucket-name-clear')
 const buckets = document.getElementById('bucket-name-list')
 const objectkeyList = document.getElementById('objectkey-list')
+const objectkeyClearBtn = document.getElementById('objectkey-name-clear')
 
 // ボタンクリック時
 bucketsNameBtn.addEventListener('click', function(event) {
   const result =  execSync('aws s3 ls --profile default')
   const arr = result.toString().split('\r\n')
   for(let i=0;i< arr.length; i++) {
-    buckets.innerHTML += '<a href=\"#\" onClick=dirr(this.innerHTML);>' + arr[i].substring(20) + '</a><br>'
+    buckets.innerHTML += '<a href=\"#\" onClick=objectkey(this.innerHTML);>' + arr[i].substring(20) + '</a><br>'
   }
   bucketsNameBtn.setAttribute('disabled', true)
   clearBtn.removeAttribute('disabled')
@@ -27,17 +28,37 @@ bucketsNameBtn.addEventListener('click', function(event) {
 clearBtn.addEventListener('click',function(event) {
   bucketsNameBtn.removeAttribute('disabled')
   clearBtn.setAttribute('disabled', true)
+  objectkeyClearBtn.setAttribute('disabled', true)
   buckets.innerHTML = ''
+  objectkeyList.innerHTML = ''
 })
 
-function dirr(hoge) {
-  console.log('hoge')
+objectkeyClearBtn.addEventListener('click',function(event) {
+  objectkeyClearBtn.setAttribute('disabled', true)
+  objectkeyList.innerHTML = ''
+})
+
+function objectkey(hoge) {
+  objectkeyClearBtn.removeAttribute('disabled')
+  objectkeyList.innerHTML = '' // 初期化
   console.log(hoge)
+
   const result =  execSync('aws s3 ls ' + hoge + ' --profile default')
-  console.log(result.toString().split('\r\n'))
+  // console.log(result.toString().split('\r\n'))
   const arr = result.toString().split('\r\n')
   for(let i=0;i< arr.length; i++) {
-    objectkeyList.innerHTML += '<a href=\"#\" onClick=dirr(this.innerHTML);>' + arr[i].substring(20) + '</a><br>'
+    let match = null
+    if(arr[i].includes('PRE')) {
+      objectkeyList.innerHTML += '<a href=\"#\" onClick=objectkey2(\"' + hoge + '\",' + 'this.innerHTML);>' + arr[i].substr(31) + '</a><br>' // リンク
+    } else {
+      objectkeyList.innerHTML += '<span>' + arr[i].substr(31) + '</span><br>' // リンク外す
+    }
   }
-
+}
+function objectkey2(bucket, key) {
+  objectkeyList.innerHTML = '' // 初期化
+  console.log('bucket')
+  console.log(bucket)
+  console.log('key')
+  console.log(key)
 }
